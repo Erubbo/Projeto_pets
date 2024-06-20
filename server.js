177fc3cda72c5a7d59757df486666adf20a6cdf5
Eros Rubbo <101677180+Erubbo@users.noreply.github.com>
2024-06-19 22:27:40 -0300

    inicio pets

---
+++ server.js
@@ -0,0 +1,82 @@
+const exp = require("constants");
+const express = require("express");
+const mysql = require("mysql2");
+
+const app = express();
+const porta = 4445;
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+// conexao com DB
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "db_adote_pet",
+});
+
+db.connect((err) => {
+  if (err) {
+    console.log("Não conectou", err);
+    return;
+  }
+  console.log("Conectado");
+});
+// Final da conexão com DB
+// ------------------------- INICIO ROTA GET----------------------------------------
+app.get(`/`, (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.get(`/pet/cadastrar`, (req, res) => {
+  res.sendFile(__dirname + "/public/gerenciar_pets.html");
+});
+
+app.get(`/pet/listar`, (req, res) => {
+  const sql = "SELECT * FROM tb_pet";
+  db.query(sql, (err, results) => {
+    if (err) {
+      return res.status(500).json({ resposta: `Erro ao consultar: ${err}` });
+    }
+    return res.status(200).json(results);
+  });
+});
+// ------------------------- FINAL ROTA GET-----------------------------------------
+
+// ------------------------- INICIO ROTA POST----------------------------------------
+app.post("/pet/cadastrar", (req, res) => {
+  let { nome, raca, porte, data_nascimento, observacao, cor, sexo, castrado } =
+    req.body;
+
+  const sql =
+    "INSERT INTO tb_pet (nome,raca,porte,data_nascimento,observacao,cor,sexo,castrado) VALUES(?,?,?,?,?,?,?,?)";
+  db.query(
+    sql,
+    [nome, raca, porte, data_nascimento, observacao, cor, sexo, castrado],
+    (err) => {
+      if (err) {
+        return res
+          .status(500)
+          .json({ resposta: `Não foi possível inserir o registro: ${err}` });
+      }
+      return res.status(200).json({ resposta: `Livro cadastrado com sucesso` });
+    }
+  );
+});
+
+// ------------------------- FINAL ROTA POST-----------------------------------------
+
+// Porta de erro
+app.use((req, res) => {
+  res.sendFile(__dirname + "/public/404.html");
+});
+
+// Servidor
+app.listen(porta, () => {
+  console.log(`Rodando na porta ${porta}`);
+});
